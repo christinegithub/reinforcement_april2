458a7676c6822d6e4ab6ea0f6cffdc04428e11a4
christinegithub <christine13@gmail.com>
2019-04-02 13:27:48 -0400

    exercise complete

---
+++ exercise.js
@@ -0,0 +1,48 @@
+let project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  dueDate : "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     dueDate: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     dueDate: "January 1, 2018"
+    },
+    {description: "compile results",
+     dueDate: "November 10, 2018"
+    },
+    {description: "version 1",
+     dueDate: "January 15, 2019"
+    },
+    {description: "revisions",
+     dueDate: "March 30, 2019"
+    },
+    {description: "version 2",
+     dueDate: "July 12, 2019"
+    },
+    {description: "final edit",
+     dueDate: "October 1, 2019"
+    },
+    {description: "final version",
+     dueDate: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     dueDate: "December 1, 2019"
+    }
+  ]
+}
+
+// # Update this dict so that each step has the name of a committee member associated with it
+// # (i.e. by adding an additional person key in each step dict), and each committee member has
+// # an equal number of tasks. Avoid typing out the committee members' names elsewhere in your code.
+
+// JS
+
+project['steps'].forEach(function(step, i) {
+  let distribute_by_three = i % 3
+  step['person'] = project['committee'][distribute_by_three];
+})
+
+console.log(project);
